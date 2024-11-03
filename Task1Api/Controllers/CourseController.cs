afd15239618581313bf9b786e1bb01d3754bc878
mohamed121helmy <mohamed121helmy@gmail.com>
2024-11-03 21:13:27 +0200

    Add project files.

---
+++ Task1Api/Controllers/CourseController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Task1Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+        private  List<Course> courses = new List<Course>()
+        {
+        new  Course(){id=1,name="Math",duration="Math1",status=true},
+         new  Course(){id=2,name="computer",duration="computer1",status=false},
+          new  Course(){id=1,name="scinse",duration="scinse1",status=true},
+
+        };
+
+        [HttpGet]
+       
+        public List<Course> GetAllCourse()
+        {
+            return courses;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        
+        public Course GetCourseById(int id)
+        {
+            return courses.Where(x => x.id == id).FirstOrDefault();
+        }
+    }
+}
---
+++ Task1Api/Course.cs
@@ -0,0 +1,10 @@
+﻿namespace Task1Api
+{
+    public class Course
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string duration { get; set; }
+        public bool status { get; set; }
+    }
+}
